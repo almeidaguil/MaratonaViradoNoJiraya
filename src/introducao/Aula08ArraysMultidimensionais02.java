d9ec3d4e8859d69b91a8efd970deebded01ab910
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-24 16:20:51 -0300

    "Arrays"

---
+++ src/introducao/Aula08ArraysMultidimensionais02.java
@@ -0,0 +1,19 @@
+package introducao;
+
+public class Aula08ArraysMultidimensionais02 {
+    public static void main(String[] args) {
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int[]{1, 2, 3};
+        arrayInt[2] = new int[]{4, 5, 6, 7, 8, 9};
+
+        for (int[] arrayBase: arrayInt){
+            System.out.println("\n-----");
+            for (int num: arrayBase){
+                System.out.print(num + " ");
+            }
+        }
+
+    }
+}
