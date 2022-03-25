25dc42d923ead3af1876cb9c414d490d1b3cf34a
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-25 15:30:41 -0300

    "Relacionamento unidirecional 1 para muitos"

--- src/orientacaoAObjetos/Gassociacao/dominio/Jogador.java
+++ src/orientacaoAObjetos/Gassociacao/dominio/Jogador.java
@@ -2,13 +2,25 @@ package orientacaoAObjetos.Gassociacao.dominio;
 
 public class Jogador {
     private String nome;
+    private Time time; //Um jogador pode fazer parte apenas de 1 time
 
     public void imprime() {
         System.out.println(this.nome);
+        if (time != null){
+            System.out.println(time.getNome());
+        }
     }
 
     public Jogador(String nome) {
         this.nome = nome;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
     }
 
     public String getNome() {
