ce2403a85c79822494028489ea4d5ea4c20ab2fc
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-25 15:07:53 -0300

    "Bloco de inicialização static"

--- src/orientacaoAObjetos/FmodificadorEstatico/dominio/Anime.java
+++ src/orientacaoAObjetos/FmodificadorEstatico/dominio/Anime.java
@@ -1,18 +1,19 @@
-package orientacaoAObjetos.EblocosInicializacao.dominio;
+package orientacaoAObjetos.FmodificadorEstatico.dominio;
 
 public class Anime {
     private String nome;
-    private int[] episodios;
-    /*1- Alocado espaço em memória para o objeto
-    * 2- Cada atritubuto de classe é criado e inicializado cm valores default
-    * 3- Bloco de inicialização é executado
-    * 4 - Construtor é executado*/
+    private static int[] episodios;
 
-    {
+    static {
+        System.out.println("Dentro do bloco de inicialização 1");
         episodios = new int[100];
         for (int i = 0; i < episodios.length; i++) {
-            episodios[i] =i+1;
+            episodios[i] = i + 1;
         }
+    }
+    static {
+        System.out.println("Dentro do bloco de inicialização 2");
+
     }
 
     public Anime(String nome) {
@@ -20,7 +21,7 @@ public class Anime {
     }
 
     public Anime() {
-        for (int episodio: this.episodios){
+        for (int episodio : Anime.episodios) {
             System.out.print(episodio + " ");
         }
     }
---
+++ src/orientacaoAObjetos/FmodificadorEstatico/teste/AnimeTest01.java
@@ -0,0 +1,11 @@
+package orientacaoAObjetos.FmodificadorEstatico.teste;
+
+import orientacaoAObjetos.FmodificadorEstatico.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
