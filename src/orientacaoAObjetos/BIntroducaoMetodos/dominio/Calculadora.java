7133bd7d34b1d1a5deaae0beaf99078b78c384c9
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-25 08:19:58 -0300

    "Introdução a metodos"

---
+++ src/orientacaoAObjetos/BIntroducaoMetodos/dominio/Calculadora.java
@@ -0,0 +1,10 @@
+package orientacaoAObjetos.BIntroducaoMetodos.dominio;
+
+public class Calculadora {
+    public void somaDoisNumeros(){
+        System.out.println(10 + 10);
+    }
+    public void sutraiDoisNumeros(){
+        System.out.println(5 - 3);
+    }
+}
---
+++ src/orientacaoAObjetos/BIntroducaoMetodos/test/CalculadoraTest01.java
@@ -0,0 +1,11 @@
+package orientacaoAObjetos.BIntroducaoMetodos.test;
+
+import orientacaoAObjetos.BIntroducaoMetodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.sutraiDoisNumeros();
+    }
+}
