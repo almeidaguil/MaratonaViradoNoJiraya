a7676e616227d828ba94adfb0ed273c515460090
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-28 14:32:48 -0300

    "Classes abstratas pt2"

--- src/orientacaoAObjetos/LclassesAbstratas/dominio/Funcionario.java
+++ src/orientacaoAObjetos/LclassesAbstratas/dominio/Funcionario.java
@@ -7,18 +7,14 @@ public abstract class Funcionario {
     public Funcionario(String nome, double salario) {
         this.nome = nome;
         this.salario = salario;
+        calculaBonus();
     }
 
-    @Override
-    public String toString() {
-        return "Funcionario{" +
-                "nome='" + nome + '\'' +
-                ", salario=" + salario +
-                '}';
-    }
+    public abstract void calculaBonus();
 }
 
 //classes abstratas foram criadas para resolver o problema de designer(desenho de codigo)
 //mas como uma relação de como ta sendo implementando o designer do seu codigo.
 //Quando a classe é declarada como abstract ela vira um "template" e nao pode ser criando um objeto do tipo da classe.
 //Classes abstratas tem que ser estendidas(extends).
+//--"traduzindo, funciona como uma interface que usei no spring boot com micro serviços"--
--- src/orientacaoAObjetos/LclassesAbstratas/teste/FuncionarioTest01.java
+++ src/orientacaoAObjetos/LclassesAbstratas/teste/FuncionarioTest01.java
@@ -7,8 +7,8 @@ import orientacaoAObjetos.LclassesAbstratas.dominio.Gerente;
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         //Funcionario funcionario = new Funcionario("Denise", 2540);
-        Gerente gerente = new Gerente("Rafael", 4534);
-        Desenvolvedor desenvolvedor = new Desenvolvedor("Isadora", 7328);
+        Gerente gerente = new Gerente("Rafael", 5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Isadora", 1200);
         System.out.println(gerente);
         System.out.println(desenvolvedor);
     }
