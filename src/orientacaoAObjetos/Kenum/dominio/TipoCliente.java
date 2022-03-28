ba5a147ca9b26bb231abda63b1f4e82e6aa99709
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-28 13:18:50 -0300

    "Enum com construtores e atributos"

--- src/orientacaoAObjetos/Kenum/dominio/TipoCliente.java
+++ src/orientacaoAObjetos/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,13 @@
 package orientacaoAObjetos.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1),
+    PESSOA_JURIDICA(2);
+
+    public final int VALOR;//sempre vem dps das enumerações
+
+    TipoCliente(int valor) {
+        this.VALOR = valor;
+    }
+
 }
--- src/orientacaoAObjetos/Kenum/teste/ClienteTest01.java
+++ src/orientacaoAObjetos/Kenum/teste/ClienteTest01.java
@@ -3,16 +3,15 @@ package orientacaoAObjetos.Kenum.teste;
 import orientacaoAObjetos.Kenum.dominio.Cliente;
 import orientacaoAObjetos.Kenum.dominio.TipoCliente;
 
+import static orientacaoAObjetos.Kenum.dominio.Cliente.*;
+
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("Orlando", TipoCliente.PESSOA_FISICA);
-        Cliente cliente1 = new Cliente("Cleia", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Marinalva", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Cezio", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente = new Cliente("Orlando", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente1 = new Cliente("Cleia", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente);
         System.out.println(cliente1);
-        System.out.println(cliente2);
-        System.out.println(cliente3);
+
     }
 }
