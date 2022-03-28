15737d62a5491e4d699fa446f8d5bd6ce76be45a
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-28 11:02:09 -0300

    "Introdução a sobrescrita do metodo ToString"

---
+++ src/orientacaoAObjetos/Isobrescrita/dominio/Anime.java
@@ -0,0 +1,24 @@
+package orientacaoAObjetos.Isobrescrita.dominio;
+
+public class Anime {
+    private String nome;
+
+    @Override //para sobrescrita
+    public String toString() {
+        return "Anime{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/orientacaoAObjetos/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package orientacaoAObjetos.Isobrescrita.test;
+
+import orientacaoAObjetos.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+
+    }
+}
