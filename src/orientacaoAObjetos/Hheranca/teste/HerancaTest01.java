b504c03a3a64b86cf3ad5639c4b5dbdff35b81ad
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-28 10:19:53 -0300

    "Explicação sobre construtores em heranças"

--- src/orientacaoAObjetos/Hheranca/teste/HerancaTest01.java
+++ src/orientacaoAObjetos/Hheranca/teste/HerancaTest01.java
@@ -9,16 +9,14 @@ public class HerancaTest01 {
         Endereco endereco = new Endereco();
         endereco.setRua("Rua das amoras - 37");
         endereco.setCep("21071-000");
-        Pessoa pessoa = new Pessoa();
-        pessoa.setNome("Yago Matos");
+        Pessoa pessoa = new Pessoa("Yago Matos");
         pessoa.setCpf("123.456.789-00");
         pessoa.setEndereco(endereco);
 
         pessoa.imprime();
 
         System.out.println("-----Funcionario-----");
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("Alfredo Torres");
+        Funcionario funcionario = new Funcionario("Alfredo Torres");
         funcionario.setCpf("098.765.432-11");
         funcionario.setEndereco(endereco);
         funcionario.setSalario(2333.87);
@@ -26,3 +24,8 @@ public class HerancaTest01 {
         funcionario.imprime();
     }
 }
+/* todas as classes em java são Objetos.
+não pode ter herança multipla no java pois não pode haver "2 pais" (extends xxxx, yyyy)
+
+toda vez que adionar um construtor na sua classe mãe, obrigatoriamente tem que alterar em todas as classes filhas.
+ */
