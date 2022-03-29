cd863defcc92a5860ae15798345aa59bb1241328
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-29 15:58:17 -0300

    "Explicação sobre tipos de variável wrapper"

---
+++ src/orientacaoAObjetos/Pwrappers/teste/WrapperTest01.java
@@ -0,0 +1,26 @@
+package orientacaoAObjetos.Pwrappers.teste;
+
+public class WrapperTest01 {
+    //tipos primitivos
+    byte byteP = 1;
+    short shortP = 1;
+    int intP = 1;
+    long longP = 1;
+    float floatP = 1;
+    double doubleP = 1;
+    char charP = 'P';
+    boolean booleanP = false;
+    //Wrappers
+    Byte byteW = 1;
+    Short shortW = 1;
+    Integer intW = 1;
+    Long longW = 1L;
+    Float floatW = 1F;
+    Double doubleW = 10D;
+    Character charW = 'W';
+    Boolean booleanW = false;
+
+
+}
+/*WRAPPERS
+ são objetos que vão encapsular os tipos primitivos*/
