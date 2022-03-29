34c0e0142dc9247231fe722e1d28941e1f0b87a9
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-29 13:35:23 -0300

    "múltiplas exceções dentro de um mesmo catch"

---
+++ src/orientacaoAObjetos/Oexception/runtime/RuntimeExceptionTest05.java
@@ -0,0 +1,31 @@
+package orientacaoAObjetos.Oexception.runtime;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest05 {
+    public static void main(String[] args) {
+        try {
+            throw new ArrayIndexOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException");
+        }
+
+        try {
+            talvezLanceException();
+        } catch (SQLException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void talvezLanceException() throws SQLException, FileNotFoundException {
+
+    }
+}
+/* REGRAS
+- não pode colocar o mais genérico antes dos mais específicos
+  */
