6ceb8114e6c7096c03251e5bc4e6eaad0a638d0e
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-30 13:26:17 -0300

    "Explicação de Date e Calendar "

---
+++ src/orientacaoAObjetos/Rdates/teste/DateTest01.java
@@ -0,0 +1,13 @@
+package orientacaoAObjetos.Rdates.teste;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        System.out.println(date);
+    }
+}
+//trabalha com long em milissegundos com inicio em 01/01/1970
+//Está completamente obsoleta
