2ba752ed6f71fff5bffc92bc242afae17b5ade19
almeidaguil <almeida.guilherme37@gmail.com>
2022-03-31 09:33:55 -0300

    "Explicação sobre LocalTime"

---
+++ src/orientacaoAObjetos/Rdates/teste/LocalTimeTest01.java
@@ -0,0 +1,19 @@
+package orientacaoAObjetos.Rdates.teste;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+public class LocalTimeTest01 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(23, 32, 12);
+        LocalTime timeNow = LocalTime.now();//hora de agora com milesegundos
+        System.out.println(time);
+        System.out.println(time.getHour());
+        System.out.println(time.getMinute());
+        System.out.println(time.getSecond());
+        System.out.println(time.get(ChronoField.HOUR_OF_DAY));
+
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
+    }
+}
