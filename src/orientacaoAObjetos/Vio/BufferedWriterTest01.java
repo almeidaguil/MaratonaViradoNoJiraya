2c48059da0413e133fd06bb0774bd340b59a642f
almeidaguil <almeida.guilherme37@gmail.com>
2022-04-01 13:37:48 -0300

    "Introdução sobre BufferedWriter."

---
+++ src/orientacaoAObjetos/Vio/BufferedWriterTest01.java
@@ -0,0 +1,23 @@
+package orientacaoAObjetos.Vio;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("Teste de escrita no arquivo usando fw.white");
+            bw.newLine();//pula linha baseado no SO ao inves de usar \n
+            bw.write("continuando a escrita");
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+//é indicado para arquivos maiores
+//tem uma escrita mais rápida, pois escreve em memória.
