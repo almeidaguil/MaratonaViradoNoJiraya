4798ed1f68c6f1ee509dcb6361f783524670ec5c
almeidaguil <almeida.guilherme37@gmail.com>
2022-04-20 07:54:42 -0300

    "List part2"

--- src/orientacaoAObjetos/yYcolecoes/test/ListTest01.java
+++ src/orientacaoAObjetos/yYcolecoes/test/ListTest01.java
@@ -6,8 +6,16 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
         nomes.add("nome1");
         nomes.add("nome2");
+        nomes2.add("avbc");
+        nomes.add("qwert");
+//        nomes.remove("nome1");//vai executar um equals para as strings e remover se achar igual
+//        nomes.remove(1); faz a remoção pelo índice
+
+//        adicionar nomes2 na list nomes
+        nomes.addAll(nomes2);
 
         for (String nome: nomes){//ForEach geralmente utilizado
             System.out.println(nome);
