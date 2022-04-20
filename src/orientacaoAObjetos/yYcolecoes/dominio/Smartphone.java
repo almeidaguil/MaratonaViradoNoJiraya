57235fb0536384926da53df0bc17338b2d7ec4f9
almeidaguil <almeida.guilherme37@gmail.com>
2022-04-20 07:17:28 -0300

    "Equals parte2"

--- src/orientacaoAObjetos/yYcolecoes/dominio/Smartphone.java
+++ src/orientacaoAObjetos/yYcolecoes/dominio/Smartphone.java
@@ -9,6 +9,15 @@ public class Smartphone {
         this.marca = marca;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
@@ -25,3 +34,10 @@ public class Smartphone {
         this.marca = marca;
     }
 }
+/*REGRAS DO EQUALS
+1- Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
+2- Simétrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
+3- Transitividade: para x, y, z, diferentes de null. Se x.equals(y) == true, e x.equals(y) == true, logo y.equals(z) == true
+4- Consistente: x.equals(x) sempre retorna true se x for diferente de null
+5- para x diferente de null, x.equals(null) tem que retornar falso
+ */
